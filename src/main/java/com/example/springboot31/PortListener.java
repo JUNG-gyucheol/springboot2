26c54e7b4fac782b4f31c90a7704074abccba344
JungGYUCHEOL <kcpooh0202@naver.com>
2020-08-01 21:35:23 +0900

    SpringBoot

---
+++ src/main/java/com/example/springboot31/PortListener.java
@@ -0,0 +1,19 @@
+package com.example.springboot31;
+
+import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
+import org.springframework.boot.web.servlet.context.ServletWebServerInitializedEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.Servlet;
+
+@Component
+public class PortListener implements ApplicationListener<ServletWebServerInitializedEvent> {
+
+    //포트번호 출력 커스터마이징
+    @Override
+    public void onApplicationEvent(ServletWebServerInitializedEvent servletWebServerInitializedEvent) {
+        ServletWebServerApplicationContext applicationContext = servletWebServerInitializedEvent.getApplicationContext();
+        System.out.println(applicationContext.getWebServer().getPort());
+    }
+}
